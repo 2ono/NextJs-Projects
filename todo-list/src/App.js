fb37184100e012bc4f4827fcdc5a5080f531feff
2ono <clt3555@gmail.com>
2023-08-30 11:26:54 +0900

    [add] Wrapper Components

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -3,22 +3,26 @@ import { Route } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupsPage from "./pages/NewMeetups";
 import FavoritesPage from "./pages/Favorites";
+import { Switch } from "react-router-dom/cjs/react-router-dom.min";
+import Layout from "./components/layout/Layout";
 
 function App() {
   // localhost: 3000/favorites/products
   // my-page.com/
   return (
-    <div>
-      <Route path="/">
-        <AllMeetupsPage />
-      </Route>
-      <Route path="/new-meetup">
-        <NewMeetupsPage />
-      </Route>
-      <Route path="/favorites">
-        <FavoritesPage />
-      </Route>
-    </div>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupsPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
